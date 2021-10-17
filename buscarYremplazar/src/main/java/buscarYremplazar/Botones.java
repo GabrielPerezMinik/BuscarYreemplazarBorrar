27daff99d3854a34fbf25b90f701959108fefe50
GabrielPerezMinik <91058454+GabrielPerezMinik@users.noreply.github.com>
2021-10-17 21:16:19 +0100

    Add files via upload

---
+++ buscarYremplazar/src/main/java/buscarYremplazar/Botones.java
@@ -0,0 +1,14 @@
+package buscarYremplazar;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Botones extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		
+	}
+
+}
